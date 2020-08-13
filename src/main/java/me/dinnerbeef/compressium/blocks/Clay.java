39227945824dbaf1faebf5da4485fa41d1a5af56
DinnerBeef <DinnerBeef18@gmail.com>
2020-08-13 10:32:36 -0500

    Updated To 1.16.2

--- src/main/java/me/dinnerbeef/compressium/blocks/Clay.java
+++ src/main/java/me/dinnerbeef/compressium/blocks/Clay.java
@@ -1,15 +1,33 @@
 package me.dinnerbeef.compressium.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.MagmaBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.color.IBlockColor;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 public class Clay extends Block
-{ public Clay()
+{
+
+	public Clay()
 	{
 		super(Properties.create(Material.CLAY).sound(SoundType.GROUND).hardnessAndResistance(0.6f).harvestTool(ToolType.SHOVEL));
-	}}
+
+
+
+
+	}
+	@Override
+	public void onBlockClicked(BlockState state, World worldIn, BlockPos pos, PlayerEntity player) {
+		super.onBlockClicked(state, worldIn, pos, player);
+
+		player.jump();
+	}
+}
 
--- src/main/java/me/dinnerbeef/compressium/blocks/Diamond.java
+++ src/main/java/me/dinnerbeef/compressium/blocks/Diamond.java
@@ -1,17 +1,11 @@
 package me.dinnerbeef.compressium.blocks;
 
-import net.minecraft.block.BeaconBlock;
+
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
+
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.data.BlockTagsProvider;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.ITag;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.TagCollection;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorldReader;
+
 import net.minecraftforge.common.ToolType;
 
 public class Diamond extends Block
